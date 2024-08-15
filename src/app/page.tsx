17edab954ad924eb1a628a94f0ce377dbb1e5563
Mehebul02 <mehebul2123@gmail.com>
2024-08-15 23:12:06 +0600

    Banner added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import BottomHeader from "@/components/header/BottomHeader";
 import Banner from "@/components/pages/home/Banner";
+import Slider from "@/components/Slider";
 
 export default function Home() {
   return (
     <main>
       <BottomHeader />
-      <Banner />
+      <Banner/>
     </main>
   );
 }
