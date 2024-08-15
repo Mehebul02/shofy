527762fd9c38867f5880a3699d95b4ecba6d0372
Mehebul02 <mehebul2123@gmail.com>
2024-08-16 00:26:33 +0600

    card desing added

--- src/components/product/Card.tsx
+++ src/components/product/Card.tsx
@@ -3,20 +3,27 @@ import Image from "next/image";
 
 const Card = () => {
     return (
-        <div className="card bg-base-100 w-96 shadow-xl">
- 
-    <Image
-      src={smartCard}
-      alt="Shoes" />
- 
-  <div className="card-body">
-    <h2 className="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
+       <div className="mt-10">
+         <div className="card bg-base-100 w-96 shadow-xl rounded-lg overflow-hidden transform transition-all duration-300 hover:scale-105">
+  <Image
+    className="w-full h-64 object-cover"
+    src={smartCard}
+    alt="Shoes"
+  />
+  <div className="card-body p-4">
+    <h2 className="card-title text-xl font-bold mb-2">Smart Watch</h2>
+    <p className="text-gray-600 mb-4">
+      If a dog chews shoes, whose shoes does he choose?
+    </p>
+    <p className="text-xl font-semibold">Price: $365</p>
     <div className="card-actions justify-end">
-      {/* <Button className="btn btn-primary">Buy Now</Button> */}
+      <button className="btn btn-primary bg-[#115061] text-white py-2 px-4 rounded-lg hover:bg-[#1a819e] focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
+        Buy Now
+      </button>
     </div>
   </div>
 </div>
+       </div>
     );
 };
 
