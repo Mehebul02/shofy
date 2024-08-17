6cf0986f10845561e8df878bbeed3e317dbd085a
Mehebul02 <mehebul2123@gmail.com>
2024-08-17 21:16:16 +0600

    card added

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -4,5 +4,8 @@ import bannerImageOne from "./banner/bannerImageOne.png";
 import smartCard from '../assets/smart-watch.jpg'
 import laptop from '../assets/laptop.jpg'
 import laptop2 from '../assets/laptop2.jpg'
+import mobile from '../assets/mobile.jpg'
+import camera  from '../assets/camera.jpg'
+import smart  from '../assets/watch.jpg'
 
-export { logo, notFound, bannerImageOne,smartCard ,laptop,laptop2};
+export { logo, notFound, bannerImageOne,smartCard ,laptop,laptop2,mobile,camera,smart };
--- src/components/product/Card.tsx
+++ src/components/product/Card.tsx
@@ -1,4 +1,4 @@
-import { laptop, laptop2, smartCard } from "@/assets";
+import { camera, laptop, laptop2, mobile, smart, smartCard } from "@/assets";
 import Image from "next/image";
 
 const Card = () => {
@@ -32,7 +32,7 @@ const Card = () => {
     alt="Shoes"
   />
   <div className="card-body p-4">
-    <h2 className="card-title text-xl font-bold mb-2">Smart Watch</h2>
+    <h2 className="card-title text-xl font-bold mb-2">Laptop 2.0</h2>
     <p className="text-gray-600 mb-4">
     A smartwatch is a versatile wearable device offering key features like...
     </p>
@@ -52,7 +52,7 @@ const Card = () => {
     alt="Shoes"
   />
   <div className="card-body p-4">
-    <h2 className="card-title text-xl font-bold mb-2">Smart Watch</h2>
+    <h2 className="card-title text-xl font-bold mb-2">Laptop 3.0</h2>
     <p className="text-gray-600 mb-4">
     A smartwatch is a versatile wearable device offering key features like...
     </p>
@@ -64,11 +64,51 @@ const Card = () => {
     </div>
   </div>
 </div>
-        {/* 1st card  */}
+        {/* 4th card  */}
          <div className="card bg-base-100  shadow-xl rounded-lg overflow-hidden transform transition-all duration-300 hover:scale-105">
   <Image
     className="w-full h-64 object-cover"
-    src={smartCard}
+    src={mobile}
+    alt="Shoes"
+  />
+  <div className="card-body p-4">
+    <h2 className="card-title text-xl font-bold mb-2">Nokia 2.4</h2>
+    <p className="text-gray-600 mb-4">
+    A smartwatch is a versatile wearable device offering key features like...
+    </p>
+    <p className="text-xl font-semibold">Price: $365</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary bg-[#115061] text-white py-2 px-4 rounded-lg hover:bg-[#1a819e] focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
+        Buy Now
+      </button>
+    </div>
+  </div>
+</div>
+        {/* 5th card  */}
+         <div className="card bg-base-100  shadow-xl rounded-lg overflow-hidden transform transition-all duration-300 hover:scale-105">
+  <Image
+    className="w-full h-64 object-cover"
+    src={camera}
+    alt="Shoes"
+  />
+  <div className="card-body p-4">
+    <h2 className="card-title text-xl font-bold mb-2">canon lbp6030</h2>
+    <p className="text-gray-600 mb-4">
+    A smartwatch is a versatile wearable device offering key features like...
+    </p>
+    <p className="text-xl font-semibold">Price: $895</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary bg-[#115061] text-white py-2 px-4 rounded-lg hover:bg-[#1a819e] focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
+        Buy Now
+      </button>
+    </div>
+  </div>
+</div>
+        {/* 4th card  */}
+         <div className="card bg-base-100  shadow-xl rounded-lg overflow-hidden transform transition-all duration-300 hover:scale-105">
+  <Image
+    className="w-full h-64 object-cover"
+    src={smart}
     alt="Shoes"
   />
   <div className="card-body p-4">
